1135e84401a45c4ff3c88e4c806f2e0a3defc4bf
Phiwe_Pohleli <pohleliphiwe@gmail.com>
2025-01-24 11:58:17 +0200

    Created a User Model and Users Table in the database.

--- CircleApp.Data/AppDbContext.cs
+++ CircleApp.Data/AppDbContext.cs
@@ -6,11 +6,9 @@ namespace CircleApp.Data
 {
     public class AppDbContext: DbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-        {
-            
-        }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options){}
 
         public DbSet<Post> Posts { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
