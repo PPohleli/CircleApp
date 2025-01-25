9176a3c6b35a257afe35a2ad29669b8b5eac534d
Phiwe_Pohleli <pohleliphiwe@gmail.com>
2025-01-25 06:07:46 +0200

    Using AppDbContext in HomeController to load Posts.

--- CircleApp/Controllers/HomeController.cs
+++ CircleApp/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
+using CircleApp.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace CircleApp.Controllers
@@ -6,14 +8,17 @@ namespace CircleApp.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly AppDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, AppDbContext contect)
         {
             _logger = logger;
+            _context = contect;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            var allPosts = await _context.Posts.Include(n => n.User).ToListAsync();
             return View();
         }
     }
