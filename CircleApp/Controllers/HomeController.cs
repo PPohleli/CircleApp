e75aacbb7e1ce88a5eebf24da5c8bec04019102f
Phiwe_Pohleli <pohleliphiwe@gmail.com>
2025-01-28 07:19:25 +0200

    Implementing Like/Unlike Action in HomeController.

--- CircleApp/Controllers/HomeController.cs
+++ CircleApp/Controllers/HomeController.cs
@@ -69,5 +69,29 @@ namespace CircleApp.Controllers
             //Redirect the user to home page
             return RedirectToAction("Index");
         }
+        public async Task<IActionResult> TogglePostLike(PostLikeVM postLikeVM)
+        {
+            int loggedInUserId = 1;
+
+            // Check if user has already liked the post
+            var like = await _context.Likes.Where(l => l.PostId == postLikeVM.PostId && l.UserId == loggedInUserId).FirstOrDefaultAsync();
+
+            if (like != null)
+            {
+                _context.Likes.Remove(like);
+                await _context.SaveChangesAsync();
+            }
+            else
+            {
+                var newLike = new Like()
+                {
+                    PostId = postLikeVM.PostId,
+                    UserId = loggedInUserId
+                };
+                await _context.Likes.AddAsync(newLike);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction("Index");
+        }
     }
 }
