3a37b8b21910126f2ea7f604e62f469ab951f92e
Phiwe_Pohleli <pohleliphiwe@gmail.com>
2025-02-01 12:08:17 +0200

    Soft Deletion of Post From the Database.

--- CircleApp.Data/Models/User.cs
+++ CircleApp.Data/Models/User.cs
@@ -11,6 +11,7 @@ namespace CircleApp.Data.Models
         public int Id { get; set; }
         public string FullName { get; set; }
         public string? ProfilePictureUrl { get; set; }
+        public bool IsDeleted { get; set; }
 
         //Navigation properties: One-Many relationship (Posts)
         public ICollection<Post> Posts { get; set; } = new List<Post>();
