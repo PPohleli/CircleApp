c52d550f41a23b37837ca1924d387ce0601b3cbf
Phiwe_Pohleli <pohleliphiwe@gmail.com>
2025-02-04 15:50:11 +0200

    Updating the ViewComponent to show Hashtag count.

--- CircleApp/ViewComponents/HashtagsViewComponent.cs
+++ CircleApp/ViewComponents/HashtagsViewComponent.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CircleApp.Controllers;
+using CircleApp.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CircleApp.ViewComponents
 {
     public class HashtagsViewComponent : ViewComponent 
     {
+        private readonly AppDbContext _context;
+
+        public HashtagsViewComponent(AppDbContext contect)
+        {
+            _context = contect;
+        }
         public async Task<IViewComponentResult> InvokeAsync()
         {
-            return View();
+            var top3Hashtags = await _context.Hashtags.Where(h => h.DateCreated >= h.DateCreated.AddDays(-7)).OrderByDescending(n => n.Count).Take(3).ToListAsync();
+            return View(top3Hashtags);
         }
     }
 }
