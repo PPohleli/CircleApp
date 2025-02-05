5614eff9d5b7d9ffc5b5bd6ea744a121efa75bdf
Phiwe_Pohleli <pohleliphiwe@gmail.com>
2025-02-05 22:05:10 +0200

    Defining the Post Service contract.

---
+++ CircleApp.Data/Services/PostService.cs
@@ -0,0 +1,58 @@
+﻿using CircleApp.Data.Models;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleApp.Data.Services
+{
+    public class PostService : IPostService
+    {
+        private readonly AppDbContext _context;
+        public PostService(AppDbContext context)
+        {
+            this._context = context;
+        }
+        public Task AddPostCommentAsync(Comment comment)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Post> CreatePostAsync(Post post, IFormFile Image)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Post>> GetAllPostsAsync(int loggedInUserId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemovePostAsync(int postId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemovePostCommentAsync(int commentId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task ReportPostAsync(int postId, int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task TogglePostFavoriteAsync(int postId, int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task TogglePostLikeAsync(int postId, int userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
