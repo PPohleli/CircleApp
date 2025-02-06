5f5be663bf8c30468ac9bb61ea095d6d6a71ab08
Phiwe_Pohleli <pohleliphiwe@gmail.com>
2025-02-06 21:28:20 +0200

    Creating a controller and an Action and a View for Favorited Posts.

---
+++ CircleApp/Controllers/FavoritesController.cs
@@ -0,0 +1,18 @@
+﻿using CircleApp.Data.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CircleApp.Controllers
+{
+    public class FavoritesController : Controller
+    {
+        private readonly IPostService _postService;
+        public FavoritesController(IPostService postService) 
+        {
+            _postService = postService;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
