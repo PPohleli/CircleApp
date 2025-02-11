024d2efa8c7a31a31f31fdad05071d89e3f44f86
Phiwe_Pohleli <pohleliphiwe@gmail.com>
2025-02-11 17:24:59 +0200

    Seeding the Application Database with Roles and Users.

---
+++ CircleApp.Data/Helpers/Constants/AppRoles.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleApp.Data.Helpers.Constants
+{
+    public static class AppRoles
+    {
+        public const string Admin = "admin";
+        public const string User = "user";
+
+        public static readonly IReadOnlyList<string> All = new[] { Admin, User };
+    }
+}
