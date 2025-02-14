0f3ca65762d352d794ff7469f3c1a071acf19686
Phiwe_Pohleli <pohleliphiwe@gmail.com>
2025-02-14 20:17:32 +0200

    Enhencing Profile Visibility with User Data.

--- CircleApp/Controllers/SettingsController.cs
+++ CircleApp/Controllers/SettingsController.cs
@@ -3,6 +3,9 @@ using CircleApp.ViewModels.Settings;
 using CircleApp.Data.Helpers.Enums;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Identity;
+using CircleApp.Data.Models;
 
 namespace CircleApp.Controllers
 {
@@ -11,18 +14,22 @@ namespace CircleApp.Controllers
     {
         private readonly IUsersService _usersService;
         private readonly IFilesService _filesService;
-        public SettingsController(IUsersService usersService, IFilesService filesService)
+        private readonly UserManager<User> _userManager;
+        public SettingsController(IUsersService usersService, IFilesService filesService, UserManager<User> userManager)
         {
             _usersService = usersService;
             _filesService = filesService;
+            _userManager = userManager;
         }
         public async Task<IActionResult> Index()
         {
-            var loggedInUser = 1;
+            var loggedInUserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
 
-            var userDb = await _usersService.GetUser(loggedInUser);
+            var userDb = await _usersService.GetUser(int.Parse(loggedInUserId));
 
-            return View(userDb);
+            var loggedInUser = await _userManager.GetUserAsync(User);
+
+            return View(loggedInUser);
         }
 
         [HttpPost]
