2547e4e418ea66b1fae6f7aa5ef662c5d7cc6451
Phiwe_Pohleli <pohleliphiwe@gmail.com>
2025-02-17 21:35:36 +0200

    Create service methods for managing Friend requests (Accept, Reject, Cancel)

--- CircleApp.Data/Services/FriendsService.cs
+++ CircleApp.Data/Services/FriendsService.cs
@@ -1,5 +1,7 @@
-﻿using CircleApp.Data.Helpers.Constants;
+﻿using Azure.Core;
+using CircleApp.Data.Helpers.Constants;
 using CircleApp.Data.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,7 +18,9 @@ namespace CircleApp.Data.Services
         {
             _context = context;
         }
-        public async Task<bool> SendRequest(int senderId, int receiverId)
+
+
+        public async Task SendRequestAsync(int senderId, int receiverId)
         {
             var request = new FriendRequest
             {
@@ -29,8 +33,41 @@ namespace CircleApp.Data.Services
             _context.FriendRequests.Add(request);
             await _context.SaveChangesAsync();
 
-            return true;
+        }
 
+        public async Task UpdateRequestAsync(int requestId, string newStatus)
+        {
+            var requestDb = await _context.FriendRequests.FirstOrDefaultAsync(n => n.Id == requestId);
+            if (requestDb != null)
+            {
+                requestDb.Status = newStatus;
+                requestDb.DateUpdated = DateTime.UtcNow;
+                _context.Update(requestDb);
+                await _context.SaveChangesAsync();
+            }
+
+            if (newStatus == FriendshipStatus.Accepted) 
+            {
+                var friendship = new Friendship
+                {
+                    SenderId = requestDb.SenderId,
+                    ReceiverId = requestDb.ReceiverId,
+                    DateCreated = DateTime.UtcNow,
+                };
+                await _context.Friendships.AddAsync(friendship);
+
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task RemoveFriendAsync(int friendshipId)
+        {
+            var friendshipDb = await _context.Friendships.FirstOrDefaultAsync(n => n.Id == friendshipId);
+            if (friendshipDb != null) 
+            {
+                _context.Friendships.Remove(friendshipDb);
+                await _context.SaveChangesAsync();
+            }
         }
     }
 }
